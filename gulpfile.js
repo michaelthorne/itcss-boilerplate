4a7ccc428ce592162ba9d8a20be5d8b379ed12d0
Michael Thorne <michael.thorne@gmail.com>
2017-11-29 07:19:20 +0200

    Improved Gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,51 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let del = require('del');
+let runSequence = require('run-sequence');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let util = require('gulp-util');
 
-gulp.task('sass', function () {
-    return gulp.src('src/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('dist'));
+// Config
+let config = {
+    dist: !!util.env.dist,
+    paths: {
+        build: 'build/',
+        dist: 'dist/',
+        src: 'src/',
+    },
+};
+
+/**
+ * Get the path of the current environment
+ * @returns {string}
+ */
+function getPath () {
+    return config.dist ? config.paths.dist : config.paths.build;
+}
+
+// Delete built files
+gulp.task('delete', function () {
+    return del([getPath()]);
 });
 
+// Compile Sass to CSS
+gulp.task('sass', function () {
+    let options = {
+        outputStyle: 'nested',
+    };
+
+    if (config.dist) {
+        options['outputStyle'] = 'compressed';
+    }
+
+    return gulp.src(config.paths.src + '**/*.scss')
+        .pipe(sass(options).on('error', sass.logError))
+        .pipe(gulp.dest(getPath() + '/css'));
+});
+
+// Build the project
+gulp.task('build', function () {
+    runSequence('delete', 'sass');
+});
+
+// Default task
 gulp.task('default', ['sass']);
