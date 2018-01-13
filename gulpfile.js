e0a6370fe52d3591ef60696f11b4da0d70da70a2
Michael Thorne <michael.thorne@gmail.com>
2018-01-13 12:05:12 +0200

    Stylelint WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,90 @@
-let browserSync = require('browser-sync');
-let del = require('del');
-let runSequence = require('run-sequence');
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let sassLint = require('gulp-sass-lint');
-let util = require('gulp-util');
+let gulp = require('gulp')
+let browserSync = require('browser-sync')
+let del = require('del')
+let runSequence = require('run-sequence')
+let stylelint = require('gulp-stylelint')
+let sass = require('gulp-sass')
+let util = require('gulp-util')
 
 // Config
 let config = {
-    dist: !!util.env.dist,
-    paths: {
-        build: 'build/',
-        dist: 'dist/',
-        src: 'src/',
-    },
-};
+  dist: !!util.env.dist,
+  paths: {
+    build: 'build/',
+    dist: 'dist/',
+    src: 'src/'
+  }
+}
 
 /**
  * Get the path of the current environment
  * @returns {string}
  */
 function getPath () {
-    return config.dist ? config.paths.dist : config.paths.build;
+  return config.dist ? config.paths.dist : config.paths.build
 }
 
 // Cleanup build files
 gulp.task('delete', function () {
-    return del([getPath()]);
-});
+  return del([getPath()])
+})
 
 // Copy assets to build
 gulp.task('html', function () {
-    return gulp.src(config.paths.src + '**/*.html', {
-        'base': config.paths.src,
-    })
-        .pipe(gulp.dest(getPath()));
-});
+  return gulp.src(config.paths.src + '**/*.html', {
+    'base': config.paths.src
+  })
+    .pipe(gulp.dest(getPath()))
+})
 
-// Lint Sass
-gulp.task('lint', function () {
-    let options = {
-        'merge-default-rules': false,
-    };
+// Lint CSS
+gulp.task('lint-scss', function () {
 
-    return gulp.src([
-        config.paths.src + '**/*.scss',
-    ])
-        .pipe(sassLint(options))
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError());
-});
+  return gulp
+    .src(config.paths.src + '**/*.scss')
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }))
+})
 
 // Compile Sass to CSS
 gulp.task('sass', function () {
-    let options = {
-        outputStyle: 'nested',
-    };
+  let options = {
+    outputStyle: 'nested'
+  }
 
-    if (config.dist) {
-        options['outputStyle'] = 'compressed';
-    }
+  if (config.dist) {
+    options['outputStyle'] = 'compressed'
+  }
 
-    return gulp.src(config.paths.src + '**/*.scss')
-        .pipe(sass(options).on('error', sass.logError))
-        .pipe(gulp.dest(getPath() + '/css'));
-});
+  return gulp.src(config.paths.src + '**/*.scss')
+    .pipe(sass(options).on('error', sass.logError))
+    .pipe(gulp.dest(getPath() + '/css'))
+})
 
 // Watch HTML and SCSS files for changes
 gulp.task('watch', function () {
-    gulp.watch(config.paths.src + '**/*.html', ['html']).on('change', browserSync.reload);
-    gulp.watch(config.paths.src + '**/*.scss', ['sass', 'lint']).on('change', browserSync.reload);
-});
+  gulp.watch(config.paths.src + '**/*.html', ['html']).on('change', browserSync.reload)
+  gulp.watch(config.paths.src + '**/*.scss', ['sass']).on('change', browserSync.reload)
+})
 
 // Start a local server
 gulp.task('server', function () {
-    if (!config.dist) {
-        browserSync.init({
-            port: 1337,
-            reloadDelay: 250,
-            server: getPath(),
-        });
-    }
-});
+  if (!config.dist) {
+    browserSync.init({
+      port: 1337,
+      reloadDelay: 250,
+      server: getPath()
+    })
+  }
+})
 
 // Build the files
 gulp.task('build', function () {
-    runSequence('delete', 'sass', 'lint', 'html');
-});
+  runSequence('delete', 'sass', 'html')
+})
 
 // Default task
-gulp.task('default', ['build', 'server', 'watch']);
+gulp.task('default', ['build', 'server', 'watch'])
